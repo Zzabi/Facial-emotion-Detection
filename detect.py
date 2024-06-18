ed369cddd002aeffc5a91c3f773114e45e256daf
Zzabi <100187147+Zzabi@users.noreply.github.com>
2024-06-18 21:31:15 +0530

    modified argparser and utils.py

--- detect.py
+++ detect.py
@@ -1,7 +1,31 @@
-import cv2
-import numpy as np
-from keras.models import load_model
+import os
+import argparse
+from utils import *
 
-model = load_model(r"models\model_file_30epochs.h5")
+parser = argparse.ArgumentParser(description="Facial emotion detector.")
 
-faceDetect = cv2.CascadeClassifier(r"models\haarcascade_frontalface_default.xml")
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument('--img', type=str, help='Path to the input image file')
+group.add_argument('--vid', type=str, help='Path to the input video file')
+group.add_argument('--cam', action='store_true', help='Use real-time data')
+
+args = parser.parse_args()
+
+if args.img:
+    print(f"Processing image from: {args.img}")
+    if not os.path.exists(args.img):
+        print("File does not exists in specified location")
+    else:
+        detect_picture(args.img)
+
+elif args.vid:
+    print(f"Processing video from: {args.vid}")
+    print(args.vid)
+    if not os.path.exists(args.vid):
+        print("File does not exists in specified location")
+    else:
+        detect_video(args.vid)
+
+elif args.cam:
+    print("Processing input from camera")
+    detect_video(0)
